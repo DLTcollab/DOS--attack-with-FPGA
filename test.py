e32193f259a8d70c59b23404b1cf053f095bb9bc
proof.K <koftwro@gmail.com>
2018-10-04 18:50:52 +0800

    Add dcurl

---
+++ test.py
@@ -0,0 +1,34 @@
+import tool
+import iota
+from iota import Iota, Address, TryteString, Transaction
+from tool import send_transfer
+
+
+
+FULLNODE = "https://tuna.iotasalad.org:14265"
+
+SEED = 'AMRWQP9BUMJALJHBXUCHOD9HFFD9LGTGEAWMJWWXSDVOF9PI9YGJAPBQLQUOMNYEQCZPGCTHGVNNAPGHA'
+
+default_address="BXEOYAONFPBGKEUQZDUZZZODHWJDWHEOYY9AENYF9VNLXZHXBOODCOTYXW9MGGINTEJPLK9AGOPTPODVX"
+
+
+
+
+
+
+
+
+i = 0
+
+while i < 3 :
+
+    trunk_and_branch = {'trunkTransaction':'IFKPAWNVCWOAGSGIVXTFAPESHPKL9GKH9NC9OMVHIBSXZBCEUFQVKVYSBDVEQTYWKOMG9FKCWOKB99999','branchTransaction':'NRVXBKNNUSASDRRFVHPNDCEOXOYGFGDTJIZKHOZRARVOFGOYUASGFZSAILVNORUQFAZURLQLBBNGA9999'}
+
+
+    send_transfer("YILLKIDATTACK","",default_address,0,trunk_and_branch,0)
+
+    print (i)
+
+    i = i + 1
+
+
