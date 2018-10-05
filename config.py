e266f3722f8a8806f5c57969382454dca4de8d77
proof.K <koftwro@gmail.com>
2018-10-05 17:04:26 +0800

    Add attack parameter

--- config.py
+++ config.py
@@ -1,21 +1,83 @@
+"""
+This file is configuration file.
+"""
+
+
 SEED = 'AMRWQP9BUMJALJHBXUCHOD9HFFD9LGTGEAWMJWWXSDVOF9PI9YGJAPBQLQUOMNYEQCZPGCTHGVNNAPGHA'
+
+"""
+SEED is defined by user.
+It should be iota seed.
+"""
+
+
+ADDRESS = "BXEOYAONFPBGKEUQZDUZZZODHWJDWHEOYY9AENYF9VNLXZHXBOODCOTYXW9MGGINTEJPLK9AGOPTPODVX"
+
+"""
+ADDRESS is defined by user.
+It should be iota transaction's address.
+"""
 
 
 # Full Node list
 
-FULLNODE = "https://tuna.iotasalad.org:14265"
+Node1 = "https://tuna.iotasalad.org:14265"
 
 
 
 # attack_init
 
-startMessage = ""
+startMessage = "TESTDOSATTACKSTART"
 
-attackType = ""
+"""
+startMessage is defined by user.
+"""
+
+attackType = "nonSolid"
+
+"""
+attackType is nonSolid or conflictTransaction.
+
+Other can't work.
+"""
+
+nonSolidTransaction = "A99999999999999999999999999999999999999999999999999999999999999999999999999999999"
+
+
+"""
+If attackType is nonSolid , user should define non-solid transaction.
+"""
+
+
+conflictTransactionAddress = ""
+
+"""
+If attackType is conflictTransaction , user should define the
+ conflict transaction's  address.
+"""
+conflictTransactionPrivateKey = ""
+
+"""
+If attackType is conflictTransaction , user should define the
+ conflict transaction's private key.
+"""
+
+conflictTransactionValue = -1000
+
+"""
+If attackType is conflictTransaction , user should define the conflict transaction's value.
+"""
+conflictTransactionReceivedAddress = ""
+
+"""
+If attackType is conflictTransaction , user should define the conflict transaction's received address.
+"""
 
 # attack_end
 
-endMessage = ""
+endMessage = "TESTDOSATTACKEND"
 
-
+"""
+endMessage is defined by user.
+"""
 
