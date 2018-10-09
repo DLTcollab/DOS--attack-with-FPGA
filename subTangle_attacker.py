b6adc017aec995b6e6e379f7e5f37924db86f48d
proof.K <koftwro@gmail.com>
2018-10-09 21:15:17 +0800

    Add select full node at random.

--- subTangle_attacker.py
+++ subTangle_attacker.py
@@ -1,1 +1,34 @@
+import random
 from tool import *
+from config import *
+
+
+
+
+# select full node at random
+
+
+fullNode = node[random.randint(0 , fullNodeNumber - 1)]
+
+api = Iota( fullNode , seed=SEED ) 
+
+print("Selected full node:" + fullNode)
+
+# search startMessage from fullNode
+
+attackStartTag = api.find_transactions(tags=[startMessage])
+
+print(attackStartTag)
+print(attackStartTag['hashes'])
+
+while str(attackStartTag['hashes']) == "":
+    attackStartTag = api.find_transactions(tags=[startMessage])
+    print(attackStartTag)
+
+
+
+# issue subTangle transaction
+
+
+
+
