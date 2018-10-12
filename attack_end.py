bda8e2fd5f0c8ecd230cb9b333aa2d4dc4197a13
proof.K <koftwro@gmail.com>
2018-10-12 16:23:42 +0800

    attack_end can issue transaction with endMessage.

--- attack_end.py
+++ attack_end.py
@@ -6,7 +6,8 @@ api = Iota(node[0], seed=SEED)
 
 # attack end
 
-
+tips = api.get_transactions_to_approve(depth=2)
 
 send_transfer(endMessage,"",ADDRESS,0,tips,0)
 
+print("End message : " + endMessage)
